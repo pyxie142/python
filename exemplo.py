5c9b6df9db9a65cb66f93aa72f466267f0375dc2
thaysferica <168362908+pyxie142@users.noreply.github.com>
2026-02-19 08:17:17 -0300

    a

--- exemplo.py
+++ exemplo.py
@@ -1,3 +1,12 @@
 print("hello world")
 import this
+print(7+4)
 print("7" + "4")
+nome = "pietra" 
+idade = 17
+peso = 48
+print(nome, idade, peso)
+nome=input ("qual seu nome")
+idade=input ("qual sua idade")
+peso=input ("qual seu peso")
+print(nome, idade, peso)
