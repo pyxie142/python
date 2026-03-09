a19ac0ce20e5fb09a4cdf1cfcdc7a6e1cba3e672
thaysferica <168362908+pyxie142@users.noreply.github.com>
2026-03-09 11:47:07 -0300

    a

--- conteudo/cont2.py
+++ conteudo/cont2.py
@@ -8,3 +8,10 @@
 # % 'resto' da divisão
 # == é igual 
 # ordem de precedencia, expressao dentro do parenteses, potenciação, multipliacação e divisão, soma e subtração
+print (5+3*2)
+print (3*5+4**2)
+print (3*(5+4)**2)
+# pow (x,y) para fazer potenciação 
+print (pow(3,4))
+print (81**(1/4))
+print ("="*6)
