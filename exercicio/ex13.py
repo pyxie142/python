0f42f757f346b9205496d3feb9e79b5c0712603f
thaysferica <168362908+pyxie142@users.noreply.github.com>
2026-03-16 12:23:42 -0300

    a

--- exercicio/ex13.py
+++ exercicio/ex13.py
@@ -2,5 +2,5 @@ largura = float(input('Largura da parede (m): '))
 altura = float(input('Altura da parede (m): '))
 area = largura * altura
 tinta = area / 2
-print(f'Sua parede tem a dimensão de {largura}x{altura} e sua área é de {area:.2f}m².')
-print(f'Para pintar essa parede, você precisará de {tinta:.2f}l de tinta.')
+print('Sua parede tem a dimensão de {}x{} e sua área é de {:.2f}m².'.format(largura,altura, area))
+print('Para pintar essa parede, você precisará de {:.2f}l de tinta.'.format(tinta))
