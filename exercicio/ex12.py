4ebb7b483585e4b2dcbddf83ad0d68ba472c04a4
thaysferica <168362908+pyxie142@users.noreply.github.com>
2026-03-16 08:17:34 -0300

    a

---
+++ exercicio/ex12.py
@@ -0,0 +1,4 @@
+carte = float (input("quanto dinheiro vc tem na carteira?"))
+dolar = 5.38
+conve = carte/dolar
+print ("vc tem {:.2F} reais em dinheiro, convertendo isso para dolar voce tem {:.2F} " .format(carte,conve))
