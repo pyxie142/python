b41cbbbb82186c9e8a164bffc3b76018d5dca22a
thaysferica <168362908+pyxie142@users.noreply.github.com>
2026-03-17 12:28:08 -0300

    a

---
+++ exercicio/ex14.py
@@ -0,0 +1,4 @@
+prod = float (input("qual preço do produto?"))
+preco = prod * 0.05
+desco = prod - preco
+print ("está com 5% de desconto, antes era {} e com o desconto agr é {}: " .format(prod,desco))
---
+++ exercicio/ex15.py
@@ -0,0 +1,4 @@
+sal = float (input("qual o salario?"))
+aume = sal * 0.15
+soma = sal + aume
+print ("está com 15% de aumento, antes era {} e com o aumento agr é {}: " .format(sal,soma))
