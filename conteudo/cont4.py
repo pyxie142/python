effc83221b62c8f09cb5791cf804a61ca4163202
pietra <168362908+pyxie142@users.noreply.github.com>
2026-03-23 12:22:08 -0300

    a

--- conteudo/cont4.py
+++ conteudo/cont4.py
@@ -1,4 +1,10 @@
-import math 
-nu = int (input("digite um numero "))
-raiz = math.sqrt(nu)
-print ("a raiz de {} é igual a {} " .format(nu,raiz))
+#import math 
+#nu = int (input("digite um numero "))
+#raiz = math.sqrt(nu)
+#print ("a raiz de {} é igual a {} " .format(nu,math.ceil(raiz)))
+#print ("a raiz de {} é igual a {} " .format(nu,math.floor(raiz)))
+
+# importando funcionalides especificas
+from math import sqrt, floor
+nu = int (input("digite numero "))
+raiz = sqrt(nu)
