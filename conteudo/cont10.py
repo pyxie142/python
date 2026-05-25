40d8ecc48a7e33a54811179ea6b125e97a0136b0
pietra <168362908+pyxie142@users.noreply.github.com>
2026-05-25 10:59:26 -0300

    a

--- conteudo/cont10.py
+++ conteudo/cont10.py
@@ -21,5 +21,8 @@
 #for c in range(i, f + 1 , p): #+1 para chegar o número que eu quero
 #    print(c)
 #print("FIM")
-
-for c in range (0,4)
+s = 0
+for c in range (0,4):
+        n = int (input("digite um valor "))
+        s = s + n 
+print (f"o somatorio de todos os valores foi:{s} ")
