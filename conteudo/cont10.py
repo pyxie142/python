d61c942096fd29551817e828b8823bd3a0d7befb
pietra <168362908+pyxie142@users.noreply.github.com>
2026-05-25 10:39:56 -0300

    a

--- conteudo/cont10.py
+++ conteudo/cont10.py
@@ -1,12 +1,25 @@
-for c in range (1,6):
-    print ("oi")
-print ("fim")
-for c in range (1,6):
-    print (c)
-print ("fim")
-for c in range (6,0,-1):
-    print (c)
-print ("fim")
-for c in range (0,6,2):
-    print (c)
-print ("fim")
+#for c in range (1,6):
+#    print ("oi")
+#print ("fim")
+#for c in range (1,6):
+#    print (c)
+#print ("fim")
+#for c in range (6,0,-1):
+#    print (c)
+#print ("fim")
+#for c in range (0,6,2):
+#    print (c)
+#print ("fim")
+
+#n = int(input("Digite um número: "))
+#for c in range(0,n+1):#+1 para chegar o número que eu quero
+#    print(c)
+#print("FIM")
+#i = int(input("Inicio: "))
+#f = int(input("Fim: "))
+#P = int(input("Passo: "))
+#for c in range(i, f + 1 , p): #+1 para chegar o número que eu quero
+#    print(c)
+#print("FIM")
+
+for c in range (0,4)
