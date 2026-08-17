8f51080731c53505bc54779001b0e9dfc78cc26b
pietra <168362908+pyxie142@users.noreply.github.com>
2026-08-17 10:24:15 -0300

    a

--- t.py
+++ t.py
@@ -1,8 +1,16 @@
-n = 0
-c = 0
-m = 1
-while n != 757:
-    n = int(input('Digite um número [757 para parar]: '))
-    c += 1
-    m *= n
-print(f'Você digitou {c} números e a multiplicação entre eles foi {m}')
+
+
+
+from random import randint
+
+print('sou seu computador')
+print('''acabei de pensar em um número entre 0 e 9
+será que você consegue adivinhar qual foi?''')
+
+palpite = int(input('qual é o seu palpite? '))
+aleatorio = randint(0, 9)
+
+if palpite == aleatorio:
+    print('você ganhou e o computador perdeu!')
+else:
+    print(f'o computador ganhou! ele pensou no número {aleatorio}')
