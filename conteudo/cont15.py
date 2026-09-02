9ad764f6afc61e855305a503b52a03c4873096e6
pietra <168362908+pyxie142@users.noreply.github.com>
2026-09-02 08:12:18 -0300

    a

--- conteudo/cont15.py
+++ conteudo/cont15.py
@@ -1,13 +1,26 @@
-materias=[]
-quimica=[]
-back=[]
-materias.append(quimica[:])
-materias.append(back)
-[[quimica,back]]
-pessoas=[['pedro',75], ['maria',19]]
-print(pessoas[0][0]) = pedro
-print(pessoas[1][1]) = 19
+# materias=[]
+# quimica=[]
+# back=[]
+# materias.append(quimica[:])
+# materias.append(back)
+# [[quimica,back]]
+# pessoas=[['pedro',75], ['maria',19]]
+# print(pessoas[0][0]) = pedro
+# print(pessoas[1][1]) = 19
 
 
-print(pessoas[1][0]) = maria 
-print(pessoas[0]) = pedro,75
+# print(pessoas[1][0]) = maria 
+# print(pessoas[0]) = pedro,75
+
+teste = []
+teste.append('pietra')
+teste.append(30)
+galera = []
+#galera.append(teste)
+galera.append(teste[:])
+teste[0] = 'Maria'
+teste[1] = 22
+#galera.append(teste) muda a lista de cima pois eu estou ligando as listas
+galera.append(teste[:]) #agora ele ta fazendo uma copia e não ligando as listas
+print(teste)
+print(galera)
