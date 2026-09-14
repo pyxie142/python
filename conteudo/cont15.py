00285ee32925ac9926ff4b8aa64a1405677a8e49
pietra <168362908+pyxie142@users.noreply.github.com>
2026-09-14 10:27:43 -0300

    a

--- conteudo/cont15.py
+++ conteudo/cont15.py
@@ -39,3 +39,12 @@ for c in range (0,3):
     galera.append(dado[:])
     dado.clear()
 print(galera)
+for p in galera:
+    if p[1] >= 18:
+        print(f'{p[0]} é maior de idade.')
+        totmai += 1
+    else:
+        print(f'{p[0]} é menor de idade.')
+        totmen += 1
+
+print(f"Temos {totmai} maiores de idade e {totmen} menores de idade.")
