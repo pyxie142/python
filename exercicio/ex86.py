0bff9b0a5e36b41ac5e869e77b5f6910140e3116
pietra <168362908+pyxie142@users.noreply.github.com>
2026-09-15 12:31:13 -0300

    a

--- exercicio/ex86.py
+++ exercicio/ex86.py
@@ -1,7 +1,7 @@
 temp = []
 princ = []
-mai = men = []
-while true:
+mai = men = 0
+while True:
     temp.append(str(input('nome')))
     temp.append(float(input('peso')))
     if len (princ)==0:
@@ -11,5 +11,19 @@ while true:
             mai = temp [1]
         if temp [1]<men:
             men = temp [1]
-        princ.append(temp[:])
-        temp. clear ()           
+    princ.append(temp[:])
+    temp. clear ()            
+    resp=str(input('quer continuar?')).lower(). strip()[0]
+    if resp == 'n':
+        break
+print(f'os dados foram {princ}')
+print(f'vc cadastrou {len(princ)} pessoas')
+print(f'o maior peso foi de {mai}kg peso de',end='')
+for p in princ:
+    if p[1] == mai:
+        print(f'{p[0]}', end='')
+print(f'o maior peso foi de {men}kg peso de',end='')
+for p in princ:
+    if p[1] == men:
+        print(f'{p[0]}', end='')
+
